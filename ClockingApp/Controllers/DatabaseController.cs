6683cdb51402da1e8a679ea61910f2ec93eb5470
rgomezr <ramon@ramongomez.xyz>
2022-11-03 09:37:34 +0100

    Added MongoDB Driver + tested connection with Atlas by getting DBs

---
+++ ClockingApp/Controllers/DatabaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace ClockingApp.Controllers
+{
+    public class DatabaseController
+    {
+        public string GetDatabases()
+        {
+            using IHost host = Host.CreateDefaultBuilder().Build();
+
+            IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
+
+            string username = config.GetValue<string>("mongoDBusername");
+            string password = config.GetValue<string>("mongoDBpass");
+
+            MongoClient client = new MongoClient(String.Format("mongodb+srv://{0}:{1}@cluster0.fltxvuu.mongodb.net/?retryWrites=true&w=majority", username, password));
+            List<string> databases = client.ListDatabaseNames().ToList();
+            return String.Join(",", databases);
+        }
+    }
+}
+
