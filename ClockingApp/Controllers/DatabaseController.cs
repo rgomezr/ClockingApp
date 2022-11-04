04cf63f546c84e7110b923e47d843f242532e9a3
rgomezr <ramon@ramongomez.xyz>
2022-11-04 19:34:21 +0100

    Created some model classes for a clocking * MongoDB Atlas working

--- ClockingApp/Controllers/DatabaseController.cs
+++ ClockingApp/Controllers/DatabaseController.cs
@@ -1,23 +1,40 @@
-﻿using System;
+﻿using ClockingApp.Models.ClockingData;
 using MongoDB.Driver;
 
 namespace ClockingApp.Controllers
 {
     public class DatabaseController
     {
-        public string GetDatabases()
+        public static string GetDatabases()
         {
-            using IHost host = Host.CreateDefaultBuilder().Build();
-
-            IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
-
-            string username = config.GetValue<string>("mongoDBusername");
-            string password = config.GetValue<string>("mongoDBpass");
-
-            MongoClient client = new MongoClient(String.Format("mongodb+srv://{0}:{1}@cluster0.fltxvuu.mongodb.net/?retryWrites=true&w=majority", username, password));
+            string username = "";
+            string password = "";
+            MongoClient client = new MongoClient(String.Format("mongodb+srv://{0}:{1}" +
+                "@cluster0.fltxvuu.mongodb.net/?retryWrites=true&w=majority", username, password));
             List<string> databases = client.ListDatabaseNames().ToList();
             return String.Join(",", databases);
         }
+
+        public string InsertClocking()
+        {
+            try { 
+            string username = "";
+            string password = "";
+            MongoClient client = new MongoClient(String.Format("mongodb+srv://{0}:{1}" +
+                "@cluster0.fltxvuu.mongodb.net/?retryWrites=true&w=majority", username, password));
+
+            var clockingsCollection = client.GetDatabase("clockingsDB").GetCollection<Clocking>("clockings");
+
+            Clocking clockingTest = new Clocking("gomezr", new WorkDay(DateTime.Now, DateTime.Now.AddHours(2)), null);
+
+            clockingsCollection.InsertOne(clockingTest);
+            } catch (Exception exception)
+            {
+                return exception.Message;
+            }
+            return "Success";
+        }
+
     }
 }
 
