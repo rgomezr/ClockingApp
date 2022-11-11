cd803206cd2c56c4d2768a9d6e67d66b59dcfabc
rgomezr <ramon@ramongomez.xyz>
2022-11-11 13:33:30 +0100

    Added basic content for testing for Home Index * Retrieving Weeks Clockings for an user and displaying it in home

--- ClockingApp/Controllers/HomeController.cs
+++ ClockingApp/Controllers/HomeController.cs
@@ -1,21 +1,31 @@
 ﻿using System.Diagnostics;
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
 using ClockingApp.Models;
+using ClockingApp.CustomServices;
+using ClockingApp.Models.ClockingData;
 
 namespace ClockingApp.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly ClockingService _clockingService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ClockingService clockingService)
     {
         _logger = logger;
+        _clockingService = clockingService;
     }
-
-    public IActionResult Index()
+    
+    public async Task<IActionResult> Index()
     {
-        return View();
+        string username = "gomezr";
+        int currentWeek = ISOWeek.GetWeekOfYear(DateTime.Now.Date);
+        List<Clocking> weekClockings = (await _clockingService._clockingRepo.FindAllAsync(clocking => clocking.Username.Equals(username) && clocking.ClockingWeek.Equals(currentWeek))).ToList();
+        Clocking? todaysClocking = weekClockings.Where(clocking => clocking.ClockingDate.Equals(DateTime.Now.Date)).FirstOrDefault();
+        ViewData["todaysClocking"] = todaysClocking;
+        return View(weekClockings);
     }
 
     public IActionResult Privacy()
--- ClockingApp/Models/ClockingData/Clocking.cs
+++ ClockingApp/Models/ClockingData/Clocking.cs
@@ -7,13 +7,15 @@ namespace ClockingApp.Models.ClockingData
     public class Clocking : Document
     {
         public string Username { get; set; } = null!;
-        public DateTime? ClockingDate { get; set; } = null!;
+        public int ClockingWeek { get; set; }
+        public DateTime ClockingDate { get; set; }
         public WorkDay WorkDay { get; set; } = null!;
         public BreakDay? BreakDay { get; set; } = null!;
 
-        public Clocking(string username, DateTime clockingDate, WorkDay workDay, BreakDay? breakDay)
+        public Clocking(string username, int clockingWeek, DateTime clockingDate, WorkDay workDay, BreakDay? breakDay)
         {
             Username = username;
+            ClockingWeek = clockingWeek;
             ClockingDate = clockingDate;
             WorkDay = workDay;
             BreakDay = breakDay;
