4225064b28d66dba47373b778806b956ce1ea54e
rgomezr <ramon@ramongomez.xyz>
2023-01-26 10:02:23 +0100

    Added css properties for summary clocking labels & data * Added formatted property in WeeklyClockingInfo to save the parsing & formatting in the view itself

--- ClockingApp/Models/ClockingData/WeeklyClockingInfo.cs
+++ ClockingApp/Models/ClockingData/WeeklyClockingInfo.cs
@@ -12,12 +12,13 @@ namespace ClockingApp.Models.ClockingData
 		public string Username => HasClockings ? WeeklyClockings.First().Username : "";
 		public double WorkingHours => HasClockings ? WeeklyClockings.Sum(clocking => clocking.WorkDay.Duration) : 0.0;
 		public int WorkingDaysCount => HasClockings ? WeeklyClockings.Count() : 0;
+		public string WorkingHours_formatted => String.Format("{0}{1}", this.WorkingHours.ToString("##.#"), "h");
 
-		/// <summary>
-		/// Public Parametrised constructor
-		/// </summary>
-		/// <param name="weeklyClockings"></param>
-		public WeeklyClockingInfo(IList<Clocking> weeklyClockings)
+        /// <summary>
+        /// Public Parametrised constructor
+        /// </summary>
+        /// <param name="weeklyClockings"></param>
+        public WeeklyClockingInfo(IList<Clocking> weeklyClockings)
 		{
 			this.WeeklyClockings = weeklyClockings;
 		}
