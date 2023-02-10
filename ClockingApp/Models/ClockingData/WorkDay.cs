03bb732f5bba6e66cdcdf16e8b3ce24d206c1585
rgomezr <ramon@ramongomez.xyz>
2023-02-10 10:18:37 +0100

    More updates in Todays View + general styling * Made rounded buttons in Todays view * Removed card-head from todays view * Showing work duration in minutes if duration is under an hour (Specified in WorkDay obj) * Not showing date within todays view as is redundant * Removed border bottom from nav bar in Layout * Made work day info a borderless table

--- ClockingApp/Models/ClockingData/WorkDay.cs
+++ ClockingApp/Models/ClockingData/WorkDay.cs
@@ -12,9 +12,10 @@ namespace ClockingApp.Models.ClockingData
         public bool IsWorkActive => (EndDate == null);
         public bool IsWorkFinished => (EndDate != null);
         [DoublePrecision(2)]
-        public double Duration => IsWorkActive ? (DateTime.Now - ((DateTime)StartDate)).TotalHours : IsWorkFinished
-            ? (((DateTime)EndDate) - ((DateTime)StartDate)).TotalHours : 0;
-        public string Duration_formatted => String.Format("{0}{1}", this.Duration.ToString("#.#"), "h");
+        public double Duration => IsWorkActive ? (DateTime.Now - StartDate).TotalHours : IsWorkFinished
+            ? ((EndDate.Value) - StartDate).TotalHours : 0;
+        public string Duration_formatted => Duration >= 1 ? String.Format("{0}{1}", this.Duration.ToString("#.#"), "h") :
+            String.Format("{0}{1}", (this.Duration * 60).ToString("##"), "m");
 
         public WorkDay(DateTime startDate, DateTime? endDate)
         {
