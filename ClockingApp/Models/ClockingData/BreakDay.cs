702660c310fb208578eef912bad9659735ba770e
rgomezr <ramon@ramongomez.xyz>
2023-02-12 13:04:36 +0100

    Took startWork and startBreak out of card - See more updates * Added badges for Work and Break so that it can be clearly differentiated * Now showing duration in minutes if duration is under an hour * From AllClockings view, cleaned first container to select date & options * Only showing table & headers of weekly clocking info when data is available * Improved styling * Fixed issue when weekly clocking info was displayed and current break was not finished

--- ClockingApp/Models/ClockingData/BreakDay.cs
+++ ClockingApp/Models/ClockingData/BreakDay.cs
@@ -6,17 +6,16 @@ namespace ClockingApp.Models.ClockingData
     public class BreakDay
     {
         [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
-        public DateTime? StartDate { get; set; } = null!;
+        public DateTime StartDate { get; set; }
         [BsonDateTimeOptions(Kind = DateTimeKind.Local)]
         public DateTime? EndDate { get; set; } = null!;
-        public bool IsBreakActive => StartDate != null && EndDate == null;
-        public bool IsBreakFinished => StartDate != null && EndDate != null;
-        [DoublePrecision(2)]
-        public double Duration => IsBreakActive ? (DateTime.Now - ((DateTime)StartDate)).TotalHours : IsBreakFinished
-            ? (((DateTime)EndDate) - ((DateTime)StartDate)).TotalMinutes : 0;
-        public string Duration_formatted => this.Duration != 0 ? String.Format("{0}{1}", this.Duration.ToString("##"), "m") : "";
+        public bool IsBreakActive => EndDate == null;
+        public bool IsBreakFinished => EndDate != null;
+        public double Duration => IsBreakActive ? (DateTime.Now - StartDate).TotalMinutes : IsBreakFinished
+            ? (EndDate.Value - StartDate).TotalMinutes : 0;
+        public string Duration_formatted => this.Duration >= 1 ? String.Format("{0}{1}", this.Duration.ToString("##"), "m") : "";
 
-        public BreakDay(DateTime? startDate, DateTime? endDate)
+        public BreakDay(DateTime startDate, DateTime? endDate)
         {
             StartDate = startDate;
             EndDate = endDate;
